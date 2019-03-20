c7e0f4fccd234f2b98361ee9f3ac868b1baf83cc
huzs <526391233@qq.com>
2019-03-20 16:59:18 +0800

    100到999之间的水仙花数

---
+++ 水仙花数.py
@@ -0,0 +1,30 @@
+'''
+7.4.	输入一个100-999之间的数，判断是否是水仙花数
+
+number = int(input("输入一个100-999之间的数:\n"));
+if number >=100 and  number <=999 :
+	#123
+	ge =  number %10;#3
+	print(ge);
+	shi = number // 10 %10;#2
+	print(shi);
+	bai = number // 100 %10;#1
+	print(bai);
+	if ge**3+shi**3+bai**3 == number:
+		print("%d这个数是水仙花数！"%number);
+	else:
+		print("%d这个数不是水仙花数！"%number);
+	'''	
+		
+for number in range(100,1000):
+	#123
+	ge =  number %10;#3
+	#print(ge);
+	shi = number // 10 %10;#2
+	#print(shi);
+	bai = number // 100 %10;#1
+	#print(bai);
+	if ge**3+shi**3+bai**3 == number:
+		print("%d这个数是水仙花数！"%number);
+	#else:
+		#print("%d这个数不是水仙花数！"%number);
